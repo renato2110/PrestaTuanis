5dcbea5ee3e60201073a68ea672b6a474fc9881e
Renato Mainieri <renato.mainieri@windriver.com>
2023-07-21 20:26:11 -0600

    Add toolbar, footer and buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
