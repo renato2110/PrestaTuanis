94a668d70c8f213ef904cedf30bdbc6f4964da2f
Fernando Rojas <ferdorom@amazon.com>
2023-07-23 10:28:37 -0600

    Adding menu

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  @Input() authenticated: boolean = false;
 
+  menuOpen: boolean = false; // Add this line
+
+  toggleMenu() { // And add this method
+    this.menuOpen = !this.menuOpen;
+  }
+
+  logout() {
+    // Add your logout logic here
+    console.log('Logout clicked');
+    this.authenticated = false; // if you want to simulate a logout
+  }
 }
