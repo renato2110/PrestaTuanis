f9021ce58351459739bb1ccac94fdfc780374e72
Fernando Rojas <ferdorom@amazon.com>
2023-07-22 21:08:55 -0600

    Fixes checkbox bug

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,22 +1,41 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.scss']
+  styleUrls: ['./checkbox.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true
+    }
+  ]
 })
-export class CheckboxComponent {
+export class CheckboxComponent implements ControlValueAccessor {
   @Input() label: string = '';
   @Input() right: boolean = false;
-  active: boolean = false;
+  value: boolean = false;
 
-  toggleActive() {
-    this.active = !this.active;
+  private onChange = (value: boolean) => {};
+  private onTouched = () => {};
+
+  writeValue(value: boolean): void {
+    this.value = value;
   }
 
-  @Output() rightChange = new EventEmitter<boolean>();
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
 
-  onCheckboxChange(event: Event) {
-    this.rightChange.emit((event.target as HTMLInputElement).checked);
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  toggleCheckbox() {
+    this.value = !this.value;
+    this.onChange(this.value);
+    this.onTouched();
   }
 }
