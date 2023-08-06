07ebaf63b441242f67919ddbc949b3098a96772a
Renato Mainieri <renato.mainieri@windriver.com>
2023-08-06 17:43:20 -0600

    Add high contrast

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,23 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const h = matchMedia("(prefers-contrast: more)");
+
+function checks () {
+	let ht;
+	
+	if ( h.matches ) {
+		ht = 'high-contrast';
+	}
+	else {
+		ht = ''
+	}
+
+  document.getElementsByTagName('body')[0].setAttribute("class", ht);
+	
+}
+
+checks();
+
+h.addListener(checks);
